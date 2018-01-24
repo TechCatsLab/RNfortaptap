989e601fc5fe990a2abe8fd354ae0f50507c75da
Glassay <31647920+Glassay@users.noreply.github.com>
2018-01-24 15:21:12 +0800

    15：21

--- app/components/Home/SingleRecommend.js
+++ app/components/Home/SingleRecommend.js
@@ -47,9 +47,42 @@ import small from '../../res/FontSize/small';
 import medium from '../../res/FontSize/medium';
 // import windowSize from '../../res/WindowSize/window';
 
-class SingleRecommend extends React.Component {
+export default class SingleRecommend extends React.Component {
   render() {
-    const { data } = this.props;
+    const data = [
+      {
+        gId: 1,
+        gicon: 'https://img.tapimg.com/market/lcs/0f944ee48f2bf78138504cc98980e508_360.png?imageView2/1/w/60/q/40/interlace/1/ignore-error/1',
+        gfrom: '来自编辑的推荐',
+        gtitle: '旅かえる',
+        gimage: 'https://img.tapimg.com/market/images/c04ab999f692a4407d64156e51613834.jpg?imageMogr2/auto-orient/thumbnail/2080x/strip/gravity/Center/crop/2080x828/format/jpg/quality/40/interlace/1',
+        ginfo: '四叶草已经收了三次了，我的蛙怎么还不回家啊……',
+      },
+      {
+        gId: 2,
+        gicon: 'https://img.tapimg.com/market/lcs/80730d5b2027f95a316d1bb674b2c48f_360.png?imageView2/1/w/60/q/40/interlace/1/ignore-error/1',
+        gfrom: '来自编辑的推荐',
+        gtitle: '边境之旅',
+        gimage: 'https://img.tapimg.com/market/images/83c8a1d5574c1dc1e9a80b019dc51394.jpg?imageMogr2/auto-orient/thumbnail/2080x/strip/gravity/Center/crop/2080x828/format/jpg/quality/40/interlace/1',
+        ginfo: '山川湖海，唯美旅途。',
+      },
+      {
+        gId: 3,
+        gicon: 'https://img.tapimg.com/market/lcs/d001b4b53e14f4c4498fe11c1564d945_360.png?imageView2/1/w/60/q/40/interlace/1/ignore-error/1',
+        gfrom: '来自编辑的推荐',
+        gtitle: '飞跃仙境',
+        gimage: 'https://img.tapimg.com/market/images/50c65228848143de6361cf52808e40b8.jpg?imageMogr2/auto-orient/thumbnail/2080x/strip/gravity/Center/crop/2080x828/format/jpg/quality/40/interlace/1',
+        ginfo: '跟随桃乐丝的梦境，找寻未知的自己。',
+      },
+      {
+        gId: 4,
+        gicon: 'https://img.tapimg.com/market/lcs/a7cd8cfafcbbed27a4e4eabb15b3179d_360.png?imageView2/1/w/60/q/40/interlace/1/ignore-error/1',
+        gfrom: '来自编辑的推荐',
+        gtitle: 'Dancing Ball!',
+        gimage: 'https://img.tapimg.com/market/images/6b5ecdb1bfc99f3fdaeea44cb4537446.jpg?imageMogr2/auto-orient/thumbnail/2080x/strip/gravity/Center/crop/2080x828/format/jpg/quality/40/interlace/1',
+        ginfo: '独特的玩法与配乐，让你体验一场充满挑战的音乐之旅！',
+      },
+    ];
     return (
       <View>
         <ScrollView>
@@ -95,9 +128,9 @@ class SingleRecommend extends React.Component {
   }
 }
 
-export default connect(state => ({
-  data: state.recommend.data,
-}))(SingleRecommend);
+// export default connect(state => ({
+//   data: state.recommend.data,
+// }))(SingleRecommend);
 
 const styles = StyleSheet.create({
   header: {
