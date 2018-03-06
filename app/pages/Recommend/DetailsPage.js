7ca21152ab50b6cf95d6dbe61beccf2f8629aac1
Glassay <31647920+Glassay@users.noreply.github.com>
2018-03-06 16:34:14 +0800

    detailsPage head color gradual change

--- app/pages/Recommend/DetailsPage.js
+++ app/pages/Recommend/DetailsPage.js
@@ -33,10 +33,8 @@ import {
   Text,
   TouchableOpacity,
   StyleSheet,
+  FlatList,
 } from 'react-native';
-import {
-  Header,
-} from 'react-native-elements';
 import Feater from 'react-native-vector-icons/Feather';
 import Entypo from 'react-native-vector-icons/Entypo';
 import VideoPlayer from 'react-native-video-controls';
@@ -52,63 +50,182 @@ class DetailsPage extends React.Component {
     header: null,
   }
 
+  constructor(props) {
+    super(props);
+    this.headerTitle = null;
+    this.header = null;
+    this.state = {
+      labels: [
+        {
+          key: 0,
+          content: '新游预约',
+        },
+        {
+          key: 1,
+          content: '单机',
+        },
+        {
+          key: 2,
+          content: '角色扮演',
+        },
+        {
+          key: 3,
+          content: '动作',
+        },
+        {
+          key: 4,
+          content: 'MOBA',
+        },
+        {
+          key: 5,
+          content: '策略',
+        },
+        {
+          key: 6,
+          content: '卡牌',
+        },
+        {
+          key: 7,
+          content: '生存',
+        },
+        {
+          key: 8,
+          content: '模拟',
+        },
+        {
+          key: 9,
+          content: '竞速',
+        },
+        {
+          key: 10,
+          content: '益智',
+        },
+        {
+          key: 11,
+          content: '二次元',
+        },
+      ],
+    };
+  }
+
+  _onScroll(event) {
+    let y = event.nativeEvent.contentOffset.y; // eslint-disable-line
+    const opacityPercent = y / Styles.Height(220);
+    if (y < Styles.Height(220)) {
+      this.headerTitle.setNativeProps({
+        style: { opacity: opacityPercent },
+      });
+      this.header.setNativeProps({
+        style: { opacity: opacityPercent },
+      });
+    } else {
+      this.header.setNativeProps({
+        style: { opacity: 1 },
+      });
+    }
+  }
+
   render() {
     return (
       <View>
-        <Header
-          backgroundColor="transparent"
-          outerContainerStyles={{
-            position: 'absolute',
-            zIndex: 1,
-            width: Styles.ScreenWidth,
-          }}
-          leftComponent={
-            <TouchableOpacity style={styles.rowHeader}>
-              <Feater
-                name="arrow-left"
-                size={IconsSize.xlarge}
-                color="white"
-                onPress={() => this.props.navigation.goBack()}
-              />
-              <Text style={styles.headerTitle}>标题asdqwrqwrqw</Text>
-            </TouchableOpacity>
-          }
-          rightComponent={
-            <View style={styles.rowHeader}>
-              <FontAwesome
-                name="exclamation-circle"
-                size={IconsSize.large}
-                color="white"
-              />
-              <Entypo
-                name="share"
-                size={IconsSize.large}
-                color="white"
-                style={{
-                  marginLeft: 15,
-                }}
+        <View style={[styles.header, { backgroundColor: 'transparent' }]}>
+          <TouchableOpacity style={styles.rowHeader}>
+            <Feater
+              name="arrow-left"
+              size={IconsSize.large}
+              color="white"
+              onPress={() => this.props.navigation.goBack()}
+            />
+            <Text style={[styles.headerTitle, { color: 'transparent' }]}>标题asdqwrqwrqw</Text>
+          </TouchableOpacity>
+          <View style={styles.rowHeader}>
+            <FontAwesome
+              name="exclamation-circle"
+              size={IconsSize.large}
+              color="white"
+            />
+            <Entypo
+              name="share"
+              size={IconsSize.large}
+              color="white"
+              style={{
+                marginLeft: 15,
+              }}
+            />
+          </View>
+        </View>
+        <View
+          ref={ref => this.header = ref} // eslint-disable-line
+          style={[styles.header, { opacity: 0 }]}
+        >
+          <TouchableOpacity style={styles.rowHeader}>
+            <Feater
+              name="arrow-left"
+              size={IconsSize.large}
+              color="white"
+              onPress={() => this.props.navigation.goBack()}
+            />
+            <Text
+              ref={ref => this.headerTitle = ref} // eslint-disable-line
+              style={[styles.headerTitle, { opacity: 0 }]}
+            >标题asdqwrqwrqw
+            </Text>
+          </TouchableOpacity>
+          <View style={styles.rowHeader}>
+            <FontAwesome
+              name="exclamation-circle"
+              size={IconsSize.large}
+              color="white"
+            />
+            <Entypo
+              name="share"
+              size={IconsSize.large}
+              color="white"
+              style={{
+                marginLeft: 15,
+              }}
+            />
+          </View>
+        </View>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          ListHeaderComponent={
+            <View style={styles.container}>
+              <VideoPlayer
+                source={{ uri: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4' }}
+                disableVolume
+                disableBack
+                disableSeekbar
+                disableFullscreen
+                paused
+                resizeMode="cover"
               />
             </View>
           }
+          data={this.state.labels}
+          renderItem={({ item }) => (
+            <Text style={styles.list}>{item.content}</Text>
+          )}
+          onScroll={this._onScroll.bind(this)}
         />
-        <View style={styles.container}>
-          <VideoPlayer
-            source={{ uri: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4' }}
-            disableVolume
-            disableBack
-            disableSeekbar
-            disableFullscreen
-            paused
-            resizeMode="cover"
-          />
-        </View>
-        <Text>Details page is coming!</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    position: 'absolute',
+    zIndex: 1,
+    width: Styles.ScreenWidth,
+    height: Styles.Height(110),
+    borderBottomWidth: 0,
+    backgroundColor: Colors.primary,
+  },
   container: {
     height: Styles.Height(330),
     width: Styles.ScreenWidth,
@@ -118,11 +235,15 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
+    marginHorizontal: Styles.Width(20),
   },
   headerTitle: {
     fontSize: FontsSize.medium,
     color: Colors.white,
     marginLeft: Styles.Width(15),
+  },
+  list: {
+    height: 100,
   },
 });
 
